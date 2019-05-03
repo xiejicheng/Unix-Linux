3c6db6c65a29b03a590b30a396cb212cc3208c6a
xiejicheng <31449070+xiejicheng@users.noreply.github.com>
2019-05-03 15:24:05 +0800

    update cp1.c

--- CH02/cp1.c
+++ CH02/cp1.c
@@ -17,24 +17,24 @@ main(int ac, char *av[])
 {
     int in_fd, out_fd, n_chars;
     char buf[BUFFERSIZE];
-
+                                                                           /* check args */
     if( ac != 3 ){
         fprintf( stderr, "usage: %s source destination\n", *av);
         exit(1);
     }
-
+                                                                          /* open files */
     if( (in_fd = open(av[1], O_RDONLY)) == -1 )
         oops("Cannot open ", av[1]);
 
     if( (out_fd = creat( av[2], COPYMODE)) == -1 )
         oops("Cannot creat", av[2]);
-
+                                                                          /* copy files */
     while( (n_chars = read(in_fd, buf, BUFFERSIZE)) >0 )
         if( write( out_fd, buf, n_chars ) != n_chars )
             oops("Write error to", av[2]);
     if( n_chars == -1 )
         oops("Read error from", av[1]);
-
+                                                                          /* close files */
     if( close(in_fd) == -1 || close(out_fd) == -1 )
         oops("Error closing files", "");
 }
