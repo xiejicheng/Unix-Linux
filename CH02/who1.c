61bda2af01143eb9faf5e3c600340c3940774c3b
xiejicheng <yijiustd@gmail.com>
2019-07-09 15:01:12 +0800

    update who1.c

--- CH02/who1.c
+++ CH02/who1.c
@@ -1,47 +1,72 @@
-/* who1.c -a first version of the who program
- *           open, read UTMP file, and show results
+/* who1.c - a first version of the who program
+ * open, read UTMP file, and show results
  */
 #include<stdio.h>
 #include<utmp.h>
 #include<fcntl.h>
 #include<unistd.h>
 #include<stdlib.h>
-int show_info();
-#define SHOWHOST                   /* include remote machine on putput */
 
+#define SHOWHOST                     /* include remote machine on output */
+void show_info(struct utmp *);
 int main()
 {
-    struct utmp  current_record;   /* read info into here */
-    int utmpfd;                    /* read from this descriptor */
-    int reclen = sizeof(current_record);
+    struct utmp current_record;      /* read info into here */
+    int utmpfd;                      /* read from this descriptor */
+    int reclen = sizeof(current_record); 
 
-    if((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1 )
+    if((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1)
     {
-        perror(UTMP_FILE);         /* UTMP_FILE is in utmp.h */
+        perror(UTMP_FILE);           /* UTMP_FILE is in utmp.h */
         exit(1);
     }
 
-    while(read(utmpfd, &current_record, reclen) == reclen )
-        show_info (&current_record);
+    while(read(utmpfd, &current_record, reclen) == reclen)
+        show_info(&current_record);
     close(utmpfd);
-    return 0;                      /* went ok */
+    return 0;                       /* went ok */
+
 }
+
+
 
 /*
  * show info()
  * displays contents of the utmp struct in human readable form
- * *note* these sizes should not be hardwired
+ *  *note* these sizes should not the be hardwired
  */
-int show_info(struct utmp *utbufp)
+void show_info(struct utmp *utbufp)
 {
-    printf("% -8.8s",utbufp -> ut_name);   /* the logname */
-    printf(" ");                           /* a space */
-    printf("% -8.8s",utbufp -> ut_line);   /* the tty */
+    printf(" % - 8.8s", utbufp -> ut_name);      /* the logname */
+    printf(" ");                                 /* a space */
+    printf(" % - 8.8s", utbufp -> ut_line);      /* the tty */
     printf(" ");
-    printf("%10ld",utbufp -> ut_time);     /* login time */
+    printf(" % 10ld", utbufp -> ut_time);        /* login time */
     printf(" ");
-#ifdef SHOWHOST
-    printf("(%s)", utbufp -> ut_host);     /* the host */
+# ifdef SHOWHOST
+    printf("( %s )"m utbufp -> ut_host);         /* the host */
 #endif
-    printf("\n");                          /*newline */
+    printf("\n");                                /* newline */
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
