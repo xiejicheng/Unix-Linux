528e93f5d36d5c8b70df85a0341086ec49623749
xiejicheng <yijiustd@gmail.com>
2019-08-11 21:28:09 +0800

    update who.c

--- CH02/who1.c
+++ CH02/who1.c
@@ -1,52 +1,50 @@
-/* who1.c - a first version of the who program
- * open, read UTMP file, and show results
+/* who1.c - who 的第一个版本
+ * 打开、读取 UTMP文件，并显示结果
  */
+
 #include<stdio.h>
 #include<utmp.h>
 #include<fcntl.h>
 #include<unistd.h>
 #include<stdlib.h>
 
-#define SHOWHOST                     /* include remote machine on output */
-void show_info(struct utmp *);
+#define SHOWHOST                                /* 包含远程机器的输出*/
+
+
 int main()
 {
-    struct utmp current_record;      /* read info into here */
-    int utmpfd;                      /* read from this descriptor */
-    int reclen = sizeof(current_record); 
+    struct utmp current_record;                 /* 在此读信息 */
+    int utmpfd;                                 /* 从描述符读取 */
+    int reclen = sizeof(current_record);
 
-    if((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1)
-    {
-        perror(UTMP_FILE);           /* UTMP_FILE is in utmp.h */
+    if((utmpfd = open(UTMP_FILE, O_RDONLY)) == -1){
+        perror( UTMP_FILE );                    /* UTMP_FILE is in utmp.h */
         exit(1);
     }
-
     while(read(utmpfd, &current_record, reclen) == reclen)
         show_info(&current_record);
     close(utmpfd);
-    return 0;                       /* went ok */
-
+    return 0;                                   /* 进展顺利 */
 }
 
-
-
-/*
+/* 
  * show info()
- * displays contents of the utmp struct in human readable form
- *  *note* these sizes should not the be hardwired
+ * 以人们可读的形式显示 utmp 结构的内容
+ * 注意，这些 siezes 不应该使用硬链接
  */
-void show_info(struct utmp *utbufp)
+
+int show_info(struct utmp *utbufp)
 {
-    printf(" % - 8.8s", utbufp -> ut_name);      /* the logname */
-    printf(" ");                                 /* a space */
-    printf(" % - 8.8s", utbufp -> ut_line);      /* the tty */
+    printf("%-8.8s", utbufp -> ut_name);      /* 登录名 */
+    printf(" ");                                /* 一个空格 */
+    printf("%-8.8s", utbufp -> ut_line);      /* 终端设备 */
     printf(" ");
-    printf(" % 10ld", utbufp -> ut_time);        /* login time */
+    printf("%10ld", utbufp -> ut_time);      /* 登录时间 */
     printf(" ");
-# ifdef SHOWHOST
-    printf("( %s )"m utbufp -> ut_host);         /* the host */
+#ifdef SHOWHOST
+    printf("(%s)", utbufp -> ut_host);          /* 主机 */
 #endif
-    printf("\n");                                /* newline */
+    printf("\n");                               /* 换行 */
 }
 
 
